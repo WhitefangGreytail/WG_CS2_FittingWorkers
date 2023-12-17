6e90764ee8d089a6ef740d32c0a1c30b4c8c5cd3
Whitefang Greytail <whitefang_greytail@yahoo.com.au>
2023-12-17 22:23:44 +0800

    Ready for a release

--- Patches/WG_CommercialAISystem.cs
+++ Patches/WG_CommercialAISystem.cs
@@ -18,9 +18,17 @@ namespace WG_FittingWorkers.Patches
 		[HarmonyPrefix]
 		static bool GetFittingWorkers_Prefix(ref int __result, BuildingData building, BuildingPropertyData properties, int level, ServiceCompanyData serviceData)
 		{
-			// This result for a new building results in a company with 2/3 of the capacity
-			__result = Mathf.CeilToInt(serviceData.m_MaxWorkersPerCell * (float)building.m_LotSize.x * (float)building.m_LotSize.y * (2.25f + 0.25f * (float)level) * properties.m_SpaceMultiplier);
-			System.Console.WriteLine("C(" + level + ") - " + __result);
+			// This result for a new building results in a company with 2/3 of the capacity. The rest of the capacity will be filled as the company grows larger
+			// properties.m_ResidentialProperties is set is the building is mixed used. Current pre mod values were fine for me, so we restore it here
+			float baseMultiplier = 2.25f;
+			float levelStep = 0.25f;
+			if (properties.m_ResidentialProperties > 0) // Mixed
+			{
+				baseMultiplier = 1f;
+				levelStep = 0.5f;
+			}
+			__result = Mathf.CeilToInt(serviceData.m_MaxWorkersPerCell * (float)building.m_LotSize.x * (float)building.m_LotSize.y * (baseMultiplier + levelStep * (float)level) * properties.m_SpaceMultiplier);
+			//System.Console.WriteLine("C(" + level + ") - " + __result);
 			return false; // Skip original
 		}
 	}
--- Patches/WG_IndustrialAISystem.cs
+++ Patches/WG_IndustrialAISystem.cs
@@ -16,7 +16,7 @@ namespace WG_FittingWorkers.Patches
 {
 	[HarmonyPatch(typeof(IndustrialAISystem), nameof(IndustrialAISystem.GetFittingWorkers))]
 	class IndustrialPatch
-	{
+	{	
 		// Could expand these to include different types of industry combinations
 		const Resource OFFICE_INDUSTRY = Resource.Software | Resource.Telecom | Resource.Financial | Resource.Media;
 
@@ -27,18 +27,17 @@ namespace WG_FittingWorkers.Patches
 			float spaceMultiplier = properties.m_SpaceMultiplier;
 
 			// properties.m_AllowedManufactured gives a bit array of flags of what the building can be used for
+			// m_AllowedManufactured represents what is allowed in the building and can span multiple industries
 			if (properties.m_AllowedManufactured == OFFICE_INDUSTRY) {
-				// m_AllowedManufactured represents what is allowed in the building and spans multiple industries
-				baseMultiplier = 3.75f;
-				if (spaceMultiplier > 1.1f) {
-					// Accounting for the taller buildings. CS2's 'height'
-					spaceMultiplier *= 1.5f;
-                }
+				baseMultiplier = 2.75f; // Offices are slightly more dense than industry... at least in game
+				// Accounting for the taller buildings. CS2's 'height' and boosting it for offices which are taller
+				// TODO - If we can change the space multipler when loading the prefab (if it actually works this way), then remove it
+				spaceMultiplier = math.pow(spaceMultiplier, 1.4f);
 			}
 
-			// This result for a new building results in a company with 2/3 of the capacity
+			// This result for a new building results in a company with 2/3 of the capacity. The rest of the capacity will be filled as the company grows larger
 			__result = Mathf.CeilToInt(processData.m_MaxWorkersPerCell * (float)building.m_LotSize.x * (float)building.m_LotSize.y * (baseMultiplier + 0.25f * (float)level) * spaceMultiplier);
-			System.Console.WriteLine("I(" + level + ") - " + __result);
+			//System.Console.WriteLine("I(" + level + ") - " + __result);
 			return false; // Skip original
 		}
 	}
